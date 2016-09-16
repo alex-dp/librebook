731f83c193666c5055f683fc1571ad186c16b631
deeepaaa <depasquale.a@tuta.io>
2016-09-16 23:47:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("submit_book").onclick = function () {
+    location.href = "submit_book.html";
+};
+
+document.getElementById("title").onclick = function () {
+    location.href = "index.html";
+};
+
+document.getElementById("donate").onclick = function () {
+	window.location.replace("https://www.paypal.me/makeitrainonme");
+};
+
+document.getElementById("foot").onclick = function () {
+	window.location.replace("https://github.com/deeepaaa/librebook");
+};
---
+++ submit_book.js
@@ -0,0 +1,7 @@
+document.getElementById("foot").onclick = function () {
+	window.location.replace("https://github.com/deeepaaa/librebook");
+};
+
+document.getElementById("title").onclick = function () {
+    location.href = "index.html";
+};
