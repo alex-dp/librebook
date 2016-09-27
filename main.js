2bb6cab9ad0b70c32f3ca4a55a6879c2626c17b7
deeepaaa <depasquale.a@tuta.io>
2016-09-27 21:16:12 +0200

    minor styling

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 document.getElementById("submit_book").onclick = function () {
-    location.href = "submit_book.html";
+    location.href = "submit_book.html"
 };
 
 document.getElementById("title").onclick = function () {
-    location.href = "index.html";
+    location.href = "index.html"
 };
 
 document.getElementById("donate").onclick = function () {
-	location.href = "https://www.paypal.me/makeitrainonme";
+	location.href = "https://www.paypal.me/makeitrainonme"
 };
 
 document.getElementById("foot").onclick = function () {
-	location.href = "https://github.com/deeepaaa/librebook";
+	location.href = "https://github.com/deeepaaa/librebook"
 };
--- submit_book.js
+++ submit_book.js
@@ -1,7 +1,25 @@
 document.getElementById("foot").onclick = function () {
-	location.href = "https://github.com/deeepaaa/librebook";
+	location.href = "https://github.com/deeepaaa/librebook"
 };
 
 document.getElementById("title").onclick = function () {
-    location.href = "index.html";
+    location.href = "index.html"
 };
+
+if (!!document.getElementById("cont")) {
+	cont = document.getElementById("cont")
+	cont.onclick = function () {
+	    alert("Puoi contattarmi all'indirizzo\n\ndpdevelopment@librebook.xyz")
+	};
+
+	cont.style.color="Blue"
+}
+
+if (!!document.getElementById("freedom")) {
+	free = document.getElementById("freedom")
+	free.onclick = function () {
+	    location.href = "https://www.gnu.org/philosophy/free-sw.html.it"
+	};
+
+	free.style.color="Blue"
+}
