fab751f98e252c3a1707a7f068e08d28a8b59f66
deeepaaa <depasquale.a@tuta.io>
2016-09-28 16:15:14 +0200

    more coherent file names

---
+++ res.php
@@ -0,0 +1,115 @@
+<html>
+<head>
+    <title>Libre-book</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" type="image/png" href="favicon.png">
+    <meta charset="UTF-8">
+</head>
+<body>
+
+<div class="center title" id="title">
+    libr<font color="CadetBlue">e-book</font>
+</div>
+
+<?php
+
+$go = 1;
+
+$servername = "localhost";
+$username = "dpdep";
+$pw_loc = "/home/dpdep/private/pw.txt";
+$password = fread(fopen($pw_loc, "r"), filesize($pw_loc));
+$password = substr($password, 0, 11);
+
+$isbn = $_GET['isbn'];
+
+if ((!is_numeric($isbn) || strlen($isbn) != 13) && $isbn != "all") {
+    echo "<div class=\"big\">L'ISBN che hai fornito non è corretto.<br></div>";
+    $go = 0;
+}
+
+$conn = mysqli_connect($servername, $username, $password);
+
+if (!$conn)
+    die("Connection failed: " . mysqli_connect_error());
+
+$sql = "USE book_entries;";
+mysqli_query($conn, $sql);
+
+$sql = "CREATE TABLE books (
+	isbn VARCHAR(13) NOT NULL PRIMARY KEY,
+	title VARCHAR(30) NOT NULL,
+	subj VARCHAR(30) NOT NULL,
+	class VARCHAR(2),
+	file_loc VARCHAR(200),
+	pic_loc VARCHAR(200),
+	up_time TIMESTAMP
+)";
+
+if (!mysqli_query($conn, $sql)); #table exists
+
+if ($isbn === 'all')
+    $sql = "SELECT * FROM books";
+else
+    $sql = "SELECT * FROM books WHERE isbn='{$isbn}';";
+
+$result = NULL;
+if($go == 1)
+    $result = $conn->query($sql);
+
+if (!is_null($result) && $result->num_rows > 0) {
+
+    while($row = $result->fetch_assoc()) {
+
+        echo "<table class=\"fixed-width\">";
+        echo "<tr><th>ISBN:</th><td>" . $row["isbn"] . "</td>";
+        echo "<tr><th>Titolo:</th><td>" . $row["title"] . "</td>";
+        echo "<tr><th>Materia:</th><td>" . $row["subj"] . "</td>";
+        echo "<tr><th>Classe:</th><td>" . $row["class"] . "</td>";
+        echo "<tr><th>Download:</th><td>" . "<a href = \"http://librebook.xyz/{$row["file_loc"]}\">(qui)</a></td>";
+
+        echo "</table><br>";
+    }
+} else echo "<div class=\"big\">Nessun risultato.</div>";
+
+if (isset($_GET['rm']))
+    $rm = $_GET['rm'];
+if (isset($_GET['pw']))
+    $pw = $_GET['pw'];
+
+if (isset($rm) && is_numeric($rm) && isset($pw) && $pw == $password) {
+    $sql = "SELECT * FROM books WHERE isbn='{$rm}';";
+
+    $result = $conn->query($sql);
+
+    if (!is_null($result) && $result->num_rows > 0)
+        while($row = $result->fetch_assoc())
+            unlink(dirname(__FILE__) . "/" . $row["file_loc"]);
+
+    $sql = "DELETE FROM books WHERE isbn={$rm};";
+
+    if($conn->query($sql))
+        echo "La richiesta di rimozione del libro {$rm} è stata eseguita.";
+
+} else if (isset($rm) && !isset($pw))
+    echo "Serve una password per rimuovere un libro!";
+
+else if (isset($pw) && $pw != $password)
+    echo "La password è errata!";
+
+$conn->close();
+
+?>
+
+<span class="bt-r footnote" id="foot">
+    DP Development 2016<br>GNU GPL3
+</span>
+
+<span class="bt-l footnote">
+    <a href="faq.php">Contatti - FAQ</a>
+</span>
+
+<script src="main.js"></script>
+
+</body>
+</html>
---
+++ sub.php
@@ -0,0 +1,115 @@
+<html>
+<head>
+	<title>Libre-book</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" type="image/png" href="favicon.png">
+	<meta charset="UTF-8">
+</head>
+<body>
+
+<div class="center title" id="title">
+    libr<font color="CadetBlue">e-book</font>
+</div>
+
+<div class="center big">
+
+<?php
+
+$target_file = "uploads/" . basename($_FILES["file_loc"]["name"]);
+$uploadOk = 1;
+$ft = pathinfo($target_file, PATHINFO_EXTENSION);
+$val_ext = array("zip", "epub", "pbd", "fb2", "pdf");
+
+$isbn = substr($_POST['isbn'], 0, 13);
+$title = substr($_POST['title'], 0, 30);
+$subj = substr($_POST['subj'], 0, 30);
+$class = $_POST['class'];
+
+$strip = array("'", "(", ")", ";");
+
+$title = str_replace($strip, "", $title);
+$subj = str_replace($strip, "", $subj);
+
+if(isset($_POST["submit"]) && !in_array($ft, $val_ext)) {
+	echo "Il file che stai cercando di caricare non sembra essere un ebook.<br>";
+	$uploadOk = 0;
+}
+
+if (!is_numeric($isbn) || strlen($isbn) != 13) {
+	echo "L'ISBN che hai fornito non è corretto.<br>";
+	$uploadOk = 0;
+}
+
+if (strlen($target_file) >= 200) {
+	$target_file = substr(str_replace($ft, "", $target_file), 0, 180) . ".{$ft}";
+}
+
+while (file_exists($target_file))
+    $target_file = str_replace($ft, "", $target_file) . rand() . ".{$ft}";
+
+if ($_FILES["file_loc"]["size"] > 500000000) {
+    echo "Carica un file più piccolo di 500MB.<br>";
+    $uploadOk = 0;
+}
+
+if ($uploadOk == 0)
+    echo "Il tuo file non è stato caricato.<br>";
+else {
+    if (move_uploaded_file($_FILES["file_loc"]["tmp_name"], $target_file)) {
+        echo "Il file ". basename($_FILES["file_loc"]["name"]). " è stato caricato.<br>";
+
+        $servername = "localhost";
+		$username = "dpdep";
+		$pw_loc = "/home/dpdep/private/pw.txt";
+		$password = fread(fopen($pw_loc, "r"), filesize($pw_loc));
+		$password = substr($password, 0, 11);
+
+		$conn = mysqli_connect($servername, $username, $password);
+
+		if (!$conn)
+		    die("Connection failed: " . mysqli_connect_error() . "<br>");
+
+		$sql = "USE book_entries;";
+		mysqli_query($conn, $sql);
+
+		$sql = "CREATE TABLE books (
+			isbn VARCHAR(13) NOT NULL PRIMARY KEY,
+			title VARCHAR(30) NOT NULL,
+			subj VARCHAR(30) NOT NULL,
+			class VARCHAR(2),
+			file_loc VARCHAR(200),
+			pic_loc VARCHAR(200),
+			up_time TIMESTAMP
+		)";
+
+		if (!mysqli_query($conn, $sql)); #table exists
+
+		$sql = "INSERT INTO books (isbn, title, subj, class, file_loc)
+		VALUES ('{$isbn}', '{$title}', '{$subj}', '{$class}', '{$target_file}');";
+
+		if ($conn->query($sql) !== TRUE)
+		    echo "C'è stato un errore nell'inserimento di questo libro nel database.<br>
+				Inviare i seguenti dettagli a dpdevelopment@librebook.xyz potrebbe essere d'aiuto.<br>
+				<b>SQL: </b>" . $sql . "<br>
+				<b>E: </b>" . $conn->error;
+
+		$conn->close();
+    } else echo "C'è stato un errore nel caricamento.<br>";
+}
+
+?>
+
+</div>
+
+<span class="bt-r footnote" id="foot">
+	DP Development 2016<br>GNU GPL3
+</span>
+
+<span class="bt-l footnote">
+	<a href="faq.php">Contatti - FAQ</a>
+</span>
+
+<script src="main.js"></script>
+
+</body>
+</html>
